3ba676699796cd3cd8d5bf47037e28c740e81c13
qcpi069 <qcpi069@pop-os.localdomain>
2020-09-10 14:38:59 -0500

    initial commit

---
+++ src/main/java/org/tonico/datewindowing/DateWindowingApplication.java
@@ -0,0 +1,13 @@
+package org.tonico.datewindowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateWindowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateWindowingApplication.class, args);
+	}
+
+}
